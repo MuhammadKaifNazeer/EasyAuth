3620380fd3f3df857a63e6dd6e53898e2894a097
Muhammad Kaif Nazeer <122388541+MuhammadKaifNazeer@users.noreply.github.com>
2024-07-26 07:52:52 +0000

    commit

---
+++ README.md
@@ -0,0 +1,46 @@
+## Documentation Template
+
+This feature-packed documentation template, built with Next.js, offers a sleek and responsive design, perfect for all your project documentation needs.
+
+<br/>
+
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+
+This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+
+## Deploy on Vercel
+
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+## Features
+
+- MDX supported
+- Syntax highlighting
+- Table of contents
+- Pagination
+- Search
+- Code line highlight & code title
+- Static site generation
+- Custom components
+- Light mode & dark mode
+- Code Switcher
+- Code copy
+- Table of content observer highlight
---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Navbar } from "@/components/navbar";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Footer } from "@/components/footer";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Get Documentation",
+  metadataBase: new URL("https://getdocumentation.vercel.app/"),
+  description:
+    "This comprehensive documentation template, crafted with Next.js and available as open-source, delivers a sleek and responsive design, tailored to meet all your project documentation requirements.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </head>
+      <body
+        className={`${GeistSans.variable} ${GeistMono.variable} font-regular`}
+        suppressHydrationWarning
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          <main className="sm:container mx-auto w-[85vw] h-auto">
+            {children}
+            <Analytics />
+          </main>
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
