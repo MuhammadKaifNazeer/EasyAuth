383cc71a7af3f9bd9c8ded3ef27db9b705b13a8f
Muhammad Kaif Nazeer <122388541+MuhammadKaifNazeer@users.noreply.github.com>
2024-07-29 16:12:31 +0000

    updated hero section

--- app/page.tsx
+++ app/page.tsx
@@ -16,19 +16,22 @@ export default function Home() {
           Follow along on GitHub{" "}
           <MoveUpRightIcon className="w-4 h-4 font-extrabold" />
         </Link>
-        <h1 className="text-4xl font-bold mb-4 sm:text-7xl">
+        <h1 className="text-3xl font-bold mb-4 sm:text-5xl lg:text-7xl">
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#41FB1B] to-[#0DDD50]">
             Effortlessly Integrate Secure{" "}
           </span>{" "}
           Authentication into Your Project
         </h1>
-        <p className="mb-8 sm:text-xl max-w-[800px] text-muted-foreground">
+        <p className="mb-6 sm:mb-8 sm:text-lg lg:text-xl max-w-[800px] text-muted-foreground">
           Pre-built authentication systems ready to copy, paste, and customize,
           making security integration fast and hassle-free.
         </p>
-        <div className="flex flex-row items-center gap-5">
-          <Link href={`/docs/${page_routes[0].href}`}>
-            <Button className="bg-gradient-to-r from-[#41FB1B] to-[#0DDD50] text-white text-lg font-semibold px-14 py-8 rounded-full hover:shadow-xl transition duration-200">
+        <div className="flex flex-row items-center justify-center gap-5 w-full">
+          <Link
+            href={`/docs/${page_routes[0].href}`}
+            className="w-full sm:w-max"
+          >
+            <Button className="bg-gradient-to-r from-[#41FB1B] to-[#0DDD50] text-white text-md sm:text-lg lg:text-xl font-semibold px-10 sm:px-12 lg:px-14 py-6 sm:py-7 md:py-8 rounded-full hover:shadow-xl transition duration-200 w-full sm:w-max">
               Get Started
             </Button>
           </Link>
